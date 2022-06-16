386282616229e90f3b380e2a3ae543b798361eac
kaguraha <278593316@qq.com>
2022-06-17 06:49:05 +0800

    Initial commit

---
+++ GLProject/GLGeometry.h
@@ -0,0 +1,9 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+class GLGeometry {
+public:
+	void createTriangle();
+
+private:
+	float *p_Vertices; //¶¥µãÊý×é
+};
---
+++ GLProject/main.cpp
@@ -0,0 +1,66 @@
+#define GLEW_STATIC
+#include<iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+void processInt(GLFWwindow* window);
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //创建窗口
+    GLFWwindow* window = glfwCreateWindow(800, 600, "MyGLWindow", NULL, NULL);
+    if (window == nullptr) {
+        printf("window is null");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        printf("glew failed.");
+        glfwTerminate();
+        return -1;
+    }
+    //设置视口绘制区域
+    glViewport(0, 0, 800, 600);
+
+    //shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+
+
+    //loop渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        //监听事件
+        processInt(window);
+        //render()
+        //清屏
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //交换前后缓冲
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+void processInt(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
